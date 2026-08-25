85c742eeda26089ca40b9ce7d252791c9b04ea05
dev-karani <edwin.k.kithinji@gmail.com>
2026-08-25 10:51:27 +0300

    feat: refactor to subtests

--- helloWorld/hello.go
+++ helloWorld/hello.go
@@ -2,9 +2,12 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello "
+const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		name = "world"
+	}
 	return englishHelloPrefix + name
 }
 func main() {
