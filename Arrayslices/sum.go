a81cf5f41bdd7cad7992285aa4fe1bc3b7744f52
dev-karani <edwin.k.kithinji@gmail.com>
2026-08-25 15:06:03 +0300

    feat: write test for sumAll variadic function and refactor

--- Arrayslices/sum.go
+++ Arrayslices/sum.go
@@ -7,3 +7,13 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
--- Arrayslices/sum_test.go
+++ Arrayslices/sum_test.go
@@ -1,6 +1,9 @@
 package arrayslices
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("array of 5 numbers", func(t *testing.T) {
@@ -25,4 +28,13 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
 }
