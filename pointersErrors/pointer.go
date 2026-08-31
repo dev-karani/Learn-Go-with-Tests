eb05d9214376935c28f0dc5238ba3c2c6e54aff1
dev-karani <edwin.k.kithinji@gmail.com>
2026-08-31 10:50:47 +0300

    feat: declare type from type and create method

--- pointersErrors/pointer.go
+++ pointersErrors/pointer.go
@@ -3,13 +3,19 @@ package pointerserrors
 import "fmt"
 
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
 	w.balance = w.balance + amount
 }
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
--- pointersErrors/wallet_test.go
+++ pointersErrors/wallet_test.go
@@ -6,15 +6,27 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
-	got := wallet.Balance()
-	fmt.Printf("address of balance intest is %p \n", &wallet.balance)
+	t.Run("Test wallet balance", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		got := wallet.Balance()
+		fmt.Printf("address of balance intest is %p \n", &wallet.balance)
 
-	want := 10
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
 
-	}
+		}
+
+	})
+	t.Run("Bitcoin String", func(t *testing.T) {
+		btc := Bitcoin(10)
+		got := btc.String()
+		want := "20 BTC"
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
