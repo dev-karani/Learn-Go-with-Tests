99d3847c83238006b222af5a849b178714ad5293
dev-karani <edwin.k.kithinji@gmail.com>
2026-08-31 13:38:35 +0300

    feat:test a map custom type with string, error return values

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,12 +5,24 @@ import (
 )
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertString(t, got, want)
+		assertString(t, got, want)
+
+	})
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+		assertString(t, err.Error(), want)
+	})
 }
 
 func assertString(t testing.TB, got, want string) {
